4f4b59899baf9a99e619a1eef0eed4e829bd4efd
dayu <117059330+yuhua2025@users.noreply.github.com>
2026-09-12 08:11:28 +0800

    Update k30_dongle_status_screen.c

--- config/boards/shields/k30_dongle/k30_dongle_status_screen.c
+++ config/boards/shields/k30_dongle/k30_dongle_status_screen.c
@@ -129,7 +129,7 @@ lv_obj_t *zmk_display_status_screen(void) {
     /* Layer label - center */
     layer_label = lv_label_create(screen);
     lv_obj_align(layer_label, LV_ALIGN_CENTER, 0, 4);
-    lv_obj_set_style_text_font(layer_label, &lv_font_montserrat_16, 0);
+    lv_obj_set_style_text_font(layer_label, &lv_font_montserrat_12, 0);
     lv_label_set_text(layer_label, "---");
 
     /* Initial update */
